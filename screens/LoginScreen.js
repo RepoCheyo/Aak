e1b93094b526b218af57cb6d87eede4ff119b064
Cheyo <bstsnwhls@gmail.com>
2023-01-13 11:54:45 -0600

    login-screen-ui-finished

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,16 +4,19 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
   StyleSheet,
+  ScrollView,
+  TouchableOpacity,
 } from 'react-native';
 import React from 'react';
 import CustomInput from '../components/CustomInput';
 import CountryPicker from 'react-native-country-picker-modal';
 import CustomButton from '../components/CustomButton';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const LoginScreen = () => {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={{backgroundColor: 'white', height: '100%'}}>
+      <ScrollView style={{backgroundColor: 'white', height: '100%', flex: 1}}>
         <View style={styles.askContainer}>
           <Text style={styles.askTitle}>¿Cual es tu número?</Text>
           <Text style={styles.askSubtitle}>
@@ -23,16 +26,18 @@ const LoginScreen = () => {
         <View style={styles.inputsContainer}>
           <View style={styles.countryContainer}>
             <CountryPicker countryCode="MX" withFilter withCallingCode />
+            <Entypo name="chevron-small-down" size={22} />
           </View>
           <View style={styles.phoneInputCont}>
             <CustomInput
               width="80%"
               selectionColor={'gray'}
               keyboardType="numeric"
+              placeholder="2246589671"
             />
           </View>
         </View>
-        <View style={{alignItems: 'center'}}>
+        <View style={{alignItems: 'center', marginTop: 20}}>
           <CustomButton
             buttonTitle="Enviar"
             color="#ffff"
@@ -40,7 +45,24 @@ const LoginScreen = () => {
             onPress={() => console.log('Enviado')}
           />
         </View>
-      </View>
+        <View style={{alignItems: 'center', marginTop: 20}}>
+          <Text style={styles.orText}>O también</Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            alignItems: 'center',
+            marginTop: 30,
+          }}>
+          <Text
+            style={{
+              color: '#0098FD',
+              fontFamily: 'Inter-Bold',
+              fontSize: 16,
+            }}>
+            Ingresa con tu E-mail
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
     </TouchableWithoutFeedback>
   );
 };
@@ -56,17 +78,18 @@ const styles = StyleSheet.create({
   inputsContainer: {
     flex: 0.1,
     flexDirection: 'row',
-    marginBottom: 15,
   },
   phoneInputCont: {
     flex: 0.9,
   },
   countryContainer: {
-    flex: 0.1,
+    flex: 0.2,
     marginLeft: 20,
+    flexDirection: 'row',
     backgroundColor: '#d9d9d9',
-    padding: 5,
-    height: 40,
+    alignItems: 'center',
+    padding: 8,
+    height: 50,
     borderRadius: 10,
   },
   askTitle: {
@@ -78,5 +101,10 @@ const styles = StyleSheet.create({
     color: '#000',
     fontSize: 14,
     fontFamily: 'Inter-Regular',
+  },
+  orText: {
+    fontSize: 15,
+    fontFamily: 'Inter-Medium',
+    color: '#a9a9a9',
   },
 });
